508b2349b87f7b0435a1cdfa937b2f7c01ac63e2
gururajo <gururajotagerimail2@gmail.com>
2024-06-14 10:44:51 +0530

    react components

--- frontend/src/Components/LearnerMap.jsx
+++ frontend/src/Components/LearnerMap.jsx
@@ -1,174 +1,157 @@
-import { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { getResponseGet } from "../lib/utils";
 import * as d3 from "d3";
 
-const LearnerMap = ({ activitiesState, learnerPosState }) => {
+const SVGComponent = ({ children }) => {
+	const svgRef = useRef(null);
+
 	useEffect(() => {
-		loadMap();
-	});
+		const svg = d3.select(svgRef.current);
+		const g = svg.select("g");
 
-	const loadMap = async () => {
-		var response = await getResponseGet("/data");
-		if (response) {
-			const data = response.data;
-			console.log(data);
-			// remove the old component test test test4
-			var svg = d3.select(".learnerMapBody");
-			console.log("this is svg", svg);
-			svg.selectAll("*").remove();
-			svg = d3
-				.select(".learnerMapBody")
-				.append("svg")
-				.attr("width", 1000)
-				.attr("height", 1000);
+		const zoom = d3
+			.zoom()
+			.scaleExtent([0.5, 5])
+			.on("zoom", (event) => {
+				g.attr("transform", event.transform);
+			});
 
-			const g = svg.append("g");
-			console.log("this is g 1", g);
-			// Define zoom behavior
-			const zoom = d3
-				.zoom()
-				.scaleExtent([0.5, 5]) // Set limits to the zoom level
-				.on("zoom", (event) => {
-					g.attr("transform", event.transform);
-				});
+		svg.call(zoom);
+	}, []);
 
-			svg.call(zoom);
+	return (
+		<svg ref={svgRef} width="auto" height="auto">
+			<g>{children}</g>
+		</svg>
+	);
+};
 
-			// g.append("rect")
-			// 	.attr("x", 0)
-			// 	.attr("y", 0)
-			// 	.attr("width", 1000) // Same as SVG width
-			// 	.attr("height", 1000) // Same as SVG height
-			// 	.attr("fill", "none")
-			// 	.attr("stroke", "black")
-			// 	.attr("stroke-width", 2);
+const GroupComponent = ({ children }) => {
+	return <>{children}</>;
+};
 
-			// Bind data to circles
-			const circles = g
-				.selectAll("circle")
-				.data(data)
-				.enter()
-				.append("a") // Make each point a link
-				.attr("xlink:href", (d) => d.video_url)
-				.attr("target", "_blank")
-				.append("circle")
-				.attr("cx", (d) => d.x * 8000 - 2800) //d.x * 1000)
-				.attr("cy", (d) => 3650 - d.y * 8000) //1000 - d.y * 1000)
-				.attr("r", 5)
-				.attr("fill", "steelblue")
-				.on("click", function (event, d) {
-					// Change color of clicked circle
-					d3.select(this).attr("fill", "orange");
+const CircleComponent = ({ data, activitiesState }) => {
+	return (
+		<a href={data.video_url} target="_blank" rel="noopener noreferrer">
+			<circle
+				cx={data.x * 8000 - 2800}
+				cy={3650 - data.y * 8000}
+				r={5}
+				fill="steelblue"
+				onClick={(event) => {
+					event.currentTarget.setAttribute("fill", "orange");
 					activitiesState[1]((activities) => [
 						...activities,
 						{
 							type: "resource",
-							name: d.name,
-							link: d.video_url,
-							time: Date(),
+							name: data.name,
+							link: data.video_url,
+							time: new Date().toString(),
 						},
 					]);
-				})
-				.on("mouseover", function (event, d) {
-					// Display name of the data point
+				}}
+				onMouseOver={(event) => {
+					const g = d3.select(event.currentTarget.parentNode);
 					g.append("text")
-						.attr("x", d3.select(this).attr("cx")) // Position text relative to the circle
-						.attr("y", d3.select(this).attr("cy") - 10) // Adjust the position of the text slightly above the circle
-						.text(d.name)
+						.attr("x", event.currentTarget.getAttribute("cx"))
+						.attr("y", event.currentTarget.getAttribute("cy") - 10)
+						.text(data.name)
 						.attr("fill", "black")
 						.attr("font-size", "12px")
 						.attr("font-family", "sans-serif")
 						.attr("id", "name-label");
-				})
-				.on("mouseout", function (event, d) {
-					// Remove the name label when mouse moves out
-					g.selectAll("text").remove();
-				});
-			console.log("this is the learnerPos", learnerPosState);
-			g.append("rect")
-				.attr("x", learnerPosState[0][0] * 1000) // Adjust x-coordinate
-				.attr("y", 1000 - learnerPosState[0][1] * 1000)
-				.attr("width", 20)
-				.attr("height", 20)
-				.attr("fill", "red")
-				.attr("id", "individual-point")
-				.on("mouseover", function (event, d) {
-					g.append("text")
-						.attr("x", d3.select(this).attr("x"))
-						.attr("y", d3.select(this).attr("y") - 10)
-						.text("Learner") // Set text content to "Learner"
-						.attr("fill", "black")
-						.attr("font-size", "12px")
-						.attr("font-family", "sans-serif")
-						.attr("id", "learner-label");
-				})
-				.on("mouseout", function (event, d) {
-					// Remove the name label when mouse moves out
-					g.selectAll("text").remove();
-				});
+				}}
+				onMouseOut={(event) => {
+					const g = d3.select(event.currentTarget.parentNode);
+					g.select("#name-label").remove();
+				}}
+			/>
+		</a>
+	);
+};
 
-			const texts = g
-				.selectAll("text")
-				.data(data)
-				.enter()
-				.append("text")
-				.attr("x", (d) => d.x * 8000 - 2800 + 10) // Adjust x-coordinate to position text beside the circle
-				.attr("y", (d) => 3650 - d.y * 8000) // Adjust y-coordinate if needed
-				.text((d) => d.index)
-				.attr("fill", "black")
-				.attr("font-size", "12px")
-				.attr("font-family", "sans-serif");
+const LearnerPositionComponent = ({ learnerPosState }) => {
+	const pos = learnerPosState[0];
+	return (
+		<rect
+			x={pos[0] * 1000}
+			y={1000 - pos[1] * 1000}
+			width={20}
+			height={20}
+			fill="red"
+			id="individual-point"
+			onMouseOver={(event) => {
+				const g = d3.select(event.currentTarget.parentNode);
+				g.append("text")
+					.attr("x", event.currentTarget.getAttribute("x"))
+					.attr("y", event.currentTarget.getAttribute("y") - 10)
+					.text("Learner")
+					.attr("fill", "black")
+					.attr("font-size", "12px")
+					.attr("font-family", "sans-serif")
+					.attr("id", "learner-label");
+			}}
+			onMouseOut={(event) => {
+				const g = d3.select(event.currentTarget.parentNode);
+				g.select("#learner-label").remove();
+			}}
+		/>
+	);
+};
 
-			console.log("this is g", g);
-		}
-	};
+const LearnerMap = ({ activitiesState, learnerPosState }) => {
+	const [data, setData] = React.useState([]);
+
+	useEffect(() => {
+		const loadMap = async () => {
+			const response = await getResponseGet("/data");
+			if (response) {
+				setData(response.data);
+			}
+		};
+
+		loadMap();
+	}, []);
 
 	return (
-		<>
-			<div className="learnerMapBody"></div>
-		</>
+		<div className="learnerMapBody">
+			<SVGComponent>
+				<GroupComponent>
+					<rect
+						x={0}
+						y={0}
+						width={1000}
+						height={1000}
+						fill="none"
+						stroke="black"
+						strokeWidth={2}
+					/>
+					{data.map((d) => (
+						<CircleComponent
+							key={d.index}
+							data={d}
+							activitiesState={activitiesState}
+						/>
+					))}
+					<LearnerPositionComponent
+						learnerPosState={learnerPosState}
+					/>
+					{data.map((d) => (
+						<text
+							key={d.index}
+							x={d.x * 8000 - 2800 + 10}
+							y={3650 - d.y * 8000}
+							fill="black"
+							fontSize="12px"
+							fontFamily="sans-serif"
+						>
+							{d.index}
+						</text>
+					))}
+				</GroupComponent>
+			</SVGComponent>
+		</div>
 	);
 };
 
 export default LearnerMap;
-
-// import React, { useState, useEffect } from "react";
-// import ReactMapGL, { Marker } from "react-map-gl";
-
-// const mapData = [
-// 	{ id: 1, latitude: 37.7749, longitude: -122.4194 },
-// 	{ id: 2, latitude: 37.7849, longitude: -122.4094 },
-// 	{ id: 3, latitude: 37.7649, longitude: -122.4294 },
-// 	{ id: 4, latitude: 37.7549, longitude: -122.4394 },
-// 	{ id: 5, latitude: 37.7449, longitude: -122.4494 },
-// ];
-// export default function LearnerMap() {
-// 	const [viewport, setViewport] = useState({
-// 		latitude: 37.7833,
-// 		longitude: -122.4167,
-// 		zoom: 13,
-// 	});
-// 	return (
-// 		<ReactMapGL
-// 			{...viewport}
-// 			width="100%"
-// 			height="400px"
-// 			mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
-// 			onViewportChange={(newViewport) => setViewport(newViewport)}
-// 		>
-// 			{mapData.map((marker) => (
-// 				<Marker
-// 					key={marker.id}
-// 					latitude={marker.latitude}
-// 					longitude={marker.longitude}
-// 				>
-// 					<img
-// 						src="https://upload.wikimedia.org/wikipedia/commons/e/ed/Map_pin_icon.svg"
-// 						alt="Marker"
-// 						style={{ width: "20px", height: "20px" }}
-// 					/>
-// 				</Marker>
-// 			))}
-// 		</ReactMapGL>
-// 	);
-// }
